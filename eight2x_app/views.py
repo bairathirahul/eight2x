af6eda19da5e51260b428b402b3fa69485e65640
Rahul Bairathi <rbairath@uncc.edu>
2018-04-23 18:55:05 -0400

    dev

--- eight2x_app/views.py
+++ eight2x_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+def login(request):
+    pass
+
+def register(request):
+    pass
+
+def dashboard(request):
+    pass
+
+def detail(request):
+    pass
+
+
+def issues(request):
+    pass
+
 # Create your views here.
